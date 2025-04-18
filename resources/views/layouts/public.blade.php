2a80257302ecd5b2e2ad401557cb9629a01e86e8
KydossK <pierre_ple@hotmail.com>
2025-04-18 03:40:27 +0200

    création favicon
    link et favicon, ico et png

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -3,6 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <title>@yield('title', 'Pelleteuse Terrassement')</title>
+
+{{-- Favicon --}}
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.png') }}">
+<link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+
+
   @vite('resources/css/app.css')
   <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
